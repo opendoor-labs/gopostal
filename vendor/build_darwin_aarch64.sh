8f47c24ab4e3cc3a9270f5b2da6d4336fd2aef51
Jonah Back <jonah.back@opendoor.com>
2025-03-03 10:37:20 -0800

    build binary for aarch64

---
+++ vendor/build_darwin_aarch64.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+echo "Installing dependencies..."
+brew update
+brew install curl autoconf automake libtool pkg-config
+
+# Libpostal c package
+echo "Cloning libpostal repo from github"
+mkdir libpostal
+git clone https://github.com/openvenues/libpostal libpostal
+
+echo "Compiling package"
+cd libpostal
+git checkout e36f8599ba652eddd1d178ca230a20569d9d0ebf
+./bootstrap.sh
+./configure
+# Note: on M1 I had to use `arch -x64_86 make` and I had to patch the checkout with some additional includes
+make
+sudo make install
+
+echo "Moving compiled files"
+cp /usr/local/lib/libpostal.a libpostal_darwin_aarch64.a
+# We are not using the header file from darwin. We are only using the header
+# file from linux as it contains less symbols (we don't need to use the language
+# classifier).
+
+# TODO: Clean up installed files.
+rm -rf libpostal
+
+echo "Successfully installed libpostal"
