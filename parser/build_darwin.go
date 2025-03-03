38ba4893aec471c24c79abd2c327590747e80fcd
Jonah Back <jonah.back@opendoor.com>
2025-03-03 10:42:19 -0800

    build binary for aarch64

--- parser/build_darwin.go
+++ parser/build_darwin.go
@@ -1,5 +1,6 @@
 package postal
 
-// #cgo LDFLAGS: ${SRCDIR}/../vendor/libpostal_darwin.a -lm
+// #cgo amd64 LDFLAGS: ${SRCDIR}/../vendor/libpostal_darwin.a -lm
+// #cgo arm64 LDFLAGS: ${SRCDIR}/../vendor/libpostal_darwin_aarch64.a -lm
 // #cgo CFLAGS: -I${SRCDIR}/..
 import "C"
