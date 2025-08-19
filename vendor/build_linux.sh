e661d1a623db4fba88bb8dcc5fb281ce20099e1f
Luis Rocha <135025+lerocha@users.noreply.github.com>
2025-08-19 16:24:48 -0700

    feat: building for linux/arm64 platform

--- vendor/build_linux.sh
+++ vendor/build_linux.sh
@@ -3,5 +3,5 @@
 docker build -t libpostal:latest .
 container_id=$(docker create libpostal:latest)
 docker cp "$container_id":/usr/local/include/libpostal/libpostal.h libpostal.h
-docker cp "$container_id":/usr/local/lib/libpostal.a libpostal_linux.a
+docker cp "$container_id":/usr/local/lib/libpostal.a libpostal_linux_amd64.a
 docker rm "$container_id"
