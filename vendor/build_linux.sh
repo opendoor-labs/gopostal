292ef69e1e725f20a096939559f9375466501031
Luis Rocha <135025+lerocha@users.noreply.github.com>
2025-08-19 16:53:04 -0700

    feat: making build_linux.sh generic for amd64 and arm64

--- vendor/build_linux.sh
+++ vendor/build_linux.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t libpostal:latest .
-container_id=$(docker create libpostal:latest)
+if [ "$(uname -m)" = "arm64" ]; then ARCH="arm64"; else ARCH="amd64"; fi
+echo "Building libpostal for linux/$ARCH platform"
+
+docker build --platform linux/$ARCH --load -t libpostal:$ARCH .
+container_id=$(docker create libpostal:$ARCH)
 docker cp "$container_id":/usr/local/include/libpostal/libpostal.h libpostal.h
-docker cp "$container_id":/usr/local/lib/libpostal.a libpostal_linux_amd64.a
+docker cp "$container_id":/usr/local/lib/libpostal.a libpostal_linux_$ARCH.a
 docker rm "$container_id"
--- vendor/build_linux_arm64.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --platform linux/arm64 --load -t libpostal:arm64 .
-container_id=$(docker create --platform linux/arm64 libpostal:arm64)
-docker cp "$container_id":/usr/local/lib/libpostal.a libpostal_linux_arm64.a
-docker rm "$container_id"
